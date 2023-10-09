2bf1fe417f4ca1c56d59559e78f6302d708be658
Eishalex2 <t.whitney.a@gmail.com>
2023-10-09 16:51:47 -0400

    Set up API call for products

--- src/shop/shop.jsx
+++ src/shop/shop.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+
 const Shop = () => {
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=6', { mode: "cors"})
+      .then((response) => response.json())
+      .then((response) => setProducts(response))
+      .catch((error) => setError(error));
+  });
+
+  
+
   return (
     <div>
-      Shop
+      <p>Shop</p>
+      {products && (
+        products.map((product) => {
+          return (
+            <>
+              <p key={product.id}>{product.title}</p>
+              <img src={product.image} alt={product.title} />
+            </>
+
+          )
+        })
+      )}
     </div>
   )
 }
